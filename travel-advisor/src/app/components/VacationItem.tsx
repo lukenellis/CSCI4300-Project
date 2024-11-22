674d9282e2d1c1125e40dc59eb3dceed1981bd98
Luke Nellis <lmn86863@uga.edu>
2024-11-22 14:15:19 -0500

    Added edit button to items

--- travel-advisor/src/app/components/VacationItem.tsx
+++ travel-advisor/src/app/components/VacationItem.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import styles from "./VacationItem.module.css"
+import Link from "next/link";
 
 const gridImg = {
     maxWidth: "100%",
@@ -18,6 +19,12 @@ type VacationProps = {
     vacation: Vacation
 }
 
+const linkStyle = {
+    textDecoration: 'underline',
+    color: 'blue'
+};
+
+
 
 const VacationItem = ({vacation}:VacationProps) => {
     return(
@@ -26,6 +33,9 @@ const VacationItem = ({vacation}:VacationProps) => {
             <Image src={vacation.image} alt="User Image" width="200" height="150" style={gridImg}/>
             <p>{vacation.location}</p>
             <p>{vacation.dates}</p>
+            <Link href={`/edit/${vacation.title}`} passHref style={linkStyle}>
+                Edit
+            </Link>
         </div>
     )
 }
---
+++ travel-advisor/src/app/edit/[title]/page.tsx
@@ -0,0 +1,80 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { useContext, useState, useEffect } from "react";
+import { ItemsContext } from "../../components/ItemsContext";
+import styles from "../../components/AddItemForm.module.css";
+import { useParams } from "next/navigation";
+
+type Vacation = {
+    title: string;
+    image: string;
+    location: string;
+    dates: string;
+};
+
+const EditItemForm = () => {
+    const { vacations, setVacations } = useContext(ItemsContext);
+    const router = useRouter();
+    const { title } = useParams();
+    let newTitle = title ? Array.isArray(title) ? title[0] : title : '';
+    const decodedTitle = decodeURI(newTitle);
+    const vacationToEdit = vacations.find((vacation) => vacation.title === decodedTitle);
+
+    const [image, setImage] = useState(vacationToEdit?.image || "");
+    const [location, setLocation] = useState(vacationToEdit?.location || "");
+    const [startDate, setStartDate] = useState(vacationToEdit?.dates.split(" to ")[0] || "");
+    const [endDate, setEndDate] = useState(vacationToEdit?.dates.split(" to ")[1] || "");
+
+    const HandleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        
+        const updatedVacations = vacations.map((vacation) =>
+            vacation.title === decodedTitle
+                ? {
+                      ...vacation,
+                      image: image,
+                      location: location,
+                      dates: `${startDate} to ${endDate}`,
+                  }
+                : vacation
+        );
+        setVacations(updatedVacations)
+
+        router.push("/dashboard");
+    };
+
+    return (
+        <div className={styles.formContainer}>
+
+        <form className={styles.tripForm} onSubmit={HandleSubmit}>
+            <div className={styles.formGroup}>
+                <label htmlFor="trip-name">Trip Name</label>
+                <input type="text" id="trip-name" placeholder="Please enter a name" value={decodedTitle} disabled/>
+                <small className={styles.errorMessage}>Please enter a name</small>
+            </div>
+
+            <div className={styles.formGroup}>
+                <label htmlFor="image">Image URL</label>
+                <input type="text" id="image" placeholder="Paste your image URL" value={image} onChange={(e) => setImage(e.target.value)} required/>
+            </div>
+
+            <div className={styles.formGroup}>
+                <label htmlFor="destination">Destination</label>
+                <input type="text" id="destination" placeholder="Type a location" value={location} onChange={(e) => setLocation(e.target.value)} required/>
+            </div>
+
+            <div className={styles.formGroup}>
+                <label htmlFor="dates">Dates</label>
+                <div className={styles.dateGroup}>
+                    <div><p>From:</p><input type="date" id="start-date" value={startDate} onChange={(e) => setStartDate(e.target.value)} required/></div>
+                    <div><p>To:</p><input type="date" id="end-date" value={endDate} onChange={(e) => setEndDate(e.target.value)} required/></div>
+                </div>
+            </div>
+
+            <button type="submit" className={styles.continueButton}>Continue</button>
+        </form>
+    </div>
+    );
+};
+
+export default EditItemForm;
